b3fc65a4e99dbc872d5a4f226eec803140d42c2b
pricej <jeffprice71@gmail.com>
2017-03-12 11:23:45 -0400

    remove unused imports and reformatting

--- jmpc-application/src/main/java/org/overwired/jmpc/application/JmpcConfiguration.java
+++ jmpc-application/src/main/java/org/overwired/jmpc/application/JmpcConfiguration.java
@@ -2,7 +2,6 @@ package org.overwired.jmpc.application;
 
 import org.apache.commons.lang3.StringUtils;
 import org.bff.javampd.MPD;
-import org.bff.javampd.exception.MPDConnectionException;
 import org.overwired.jmpc.domain.app.Track;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Configure the JukeboxMPC application.
@@ -24,8 +21,8 @@ public class JmpcConfiguration {
 
     @Bean
     public MPD.Builder mpdBuilder(@Value("${mpd.host}") String host,
-                   @Value("${mpd.port}") int port,
-                   @Value("${mpd.password:}") String password) throws UnknownHostException {
+                                  @Value("${mpd.port}") int port,
+                                  @Value("${mpd.password:}") String password) throws UnknownHostException {
 
         LOGGER.debug("returning MPD.Builder: host='{}', port={}, password='{}", host, port, password);
         return new MPD.Builder().server(host).port(port).password(StringUtils.trimToNull(password));
